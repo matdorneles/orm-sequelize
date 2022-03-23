47b795cc1d82f2e5fbcac0b911a9897c64467660
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-03-23 17:02:58 -0300

    Iniciando projeto

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.get("/teste", (req, res) => {
+  res.status(200).send({ mensagem: "Boas-vindas" });
+});
+
+app.listen(port, () => {
+  console.log(`servidor está rodando na porta ${port}`);
+});
+
+export default app;
